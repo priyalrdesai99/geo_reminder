f65dd297618a16a8834d74857a92ec835c665cff
verikv <vvekaria@asu.edu>
2023-11-30 01:15:15 -0700

    Maps UI with Directions complete

--- app/src/main/java/com/example/geo_reminder/MainActivity.java
+++ app/src/main/java/com/example/geo_reminder/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.geo_reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText SourceText = findViewById(R.id.src);
+        EditText DestinationText = findViewById(R.id.dest);
+        Button button = findViewById(R.id.submitBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String src = SourceText.getText().toString().trim();
+                String dest = DestinationText.getText().toString().trim();
+                if(src.equals("") && dest.equals("")){
+                    Toast.makeText(getApplicationContext(),"Enter Both Source and Destination",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(v.getContext(), MapsActivity.class);
+                    intent.putExtra("SOURCE",src);
+                    intent.putExtra("DESTINATION",dest);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
