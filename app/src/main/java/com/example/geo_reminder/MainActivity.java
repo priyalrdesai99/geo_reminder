0e56507d7003a6f6f39cf367ea8cb9e2c2b46433
Adit Patel <60565128+aditpatel04@users.noreply.github.com>
2023-11-30 00:30:13 -0700

    Requirements for UI Design

--- app/src/main/java/com/example/geo_reminder/MainActivity.java
+++ app/src/main/java/com/example/geo_reminder/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
+    private LogInFragment lif;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        lif = new LogInFragment();
+        transaction.replace(R.id.flMain, lif);
+        transaction.commit();
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            lif = new LogInFragment();
+            transaction.replace(R.id.flMain, lif);
+            transaction.commit();
+        }
     }
 }
