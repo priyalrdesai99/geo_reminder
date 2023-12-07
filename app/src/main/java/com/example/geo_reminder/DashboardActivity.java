1664334c5a3c052debcebbee2029a8f1e6aaf25d
Adit Patel <60565128+aditpatel04@users.noreply.github.com>
2023-12-07 15:27:11 -0700

    List Reminder

--- app/src/main/java/com/example/geo_reminder/DashboardActivity.java
+++ app/src/main/java/com/example/geo_reminder/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.geo_reminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -7,26 +8,42 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.LayoutInflater;
-import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.material.textfield.TextInputEditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    AppCompatButton add_rem,add;
-    EditText get_item,get_desc;
+    AppCompatButton add_rem, add;
+    EditText get_item, get_desc;
+    TextView tv_item;
+
+    String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         add_rem = findViewById(R.id.add_reminder);
+        tv_item = findViewById(R.id.tv_items);
         Dialog dialog = new Dialog(DashboardActivity.this);
 
         add_rem.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +56,7 @@ public class DashboardActivity extends AppCompatActivity {
                 dialog.setCancelable(true);
 
                 Spinner spinner = (Spinner) dialog.findViewById(R.id.category_spinner);
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(DashboardActivity.this,R.array.categories, android.R.layout.simple_spinner_item);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(DashboardActivity.this, R.array.categories, android.R.layout.simple_spinner_item);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
 
@@ -47,12 +64,11 @@ public class DashboardActivity extends AppCompatActivity {
                 get_desc = dialog.findViewById(R.id.edt_desc);
                 add = dialog.findViewById(R.id.add_btn);
 
-                //String selectedSpinnerItem = spinner.getSelectedItem().toString();
                 add.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
-                        String userId = getIntent().getStringExtra("KEY");
+                        userId = getIntent().getStringExtra("KEY");
 
                         String itemName = get_item.getText().toString();
                         String description = get_desc.getText().toString();
@@ -69,6 +85,33 @@ public class DashboardActivity extends AppCompatActivity {
                         toDoItemReference.child(itemId).setValue(toDoItem);
 
                         Toast.makeText(DashboardActivity.this, " Successfully Added!!", Toast.LENGTH_SHORT).show();
+                        Query query = toDoItemReference.orderByKey();
+
+                        query.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                List<String> ans = new ArrayList<>();
+                                String x = "";
+                                int i = 1;
+                                for (DataSnapshot data : snapshot.getChildren()) {
+
+                                    String category = (String) data.child("category").getValue();
+                                    String itemname = (String) data.child("itemName").getValue();
+                                    String desc = (String) data.child("description").getValue();
+                                    String temp = String.valueOf(i)+". "+category+" "+itemname+" "+desc;
+                                    x+=temp+"\n";
+                                    System.out.println(temp);
+                                    ans.add(temp);
+                                    i+=1;
+                                }
+                                System.out.println(ans);
+                                tv_item.setText(x);
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
                         dialog.dismiss();
 
                     }
@@ -78,3 +121,5 @@ public class DashboardActivity extends AppCompatActivity {
         });
     }
 }
+
+
